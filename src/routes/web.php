0e73dff04bc561562faa7a06dc15e688ef7ea4d4
NakagawaTakato <ntakato6231@gmail.com>
2024-05-08 19:52:27 +0900

    15

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,13 +19,15 @@ Route::get('/', [ShopController::class, 'index']);
 Route::get('/search', [Shop_allController::class, 'search']);
 Route::post('/register/thanks', [ShopController::class, 'thanks']);
 Route::post('/login/menu_two', [ShopController::class, 'menu_two']);
-Route::get('/shop_all', [Shop_allController::class, 'shop_all']);
-Route::post('/shop_all/shop_detail', [Shop_allController::class, 'shop_detail']);
-Route::post('/shop_all/shop_detail/shop_detail_two', [Shop_allController::class, 'shop_detail_two']);
-Route::post('/done', [Shop_allController::class, 'done']);
-Route::post('/like_thanks', [Shop_allController::class, 'like_thanks']);
-Route::delete('/delete', [Shop_allController::class, 'delete']);
+Route::middleware('auth')->group(function () {
+    Route::get('/shop_all', [Shop_allController::class, 'shop_all']);
+    Route::post('/shop_all/shop_detail', [Shop_allController::class, 'shop_detail']);
+    Route::post('/shop_all/shop_detail/shop_detail_two', [Shop_allController::class, 'shop_detail_two']);
+    Route::post('/done', [Shop_allController::class, 'done']);
+    Route::post('/like_thanks', [Shop_allController::class, 'like_thanks']);
+    Route::delete('/delete', [Shop_allController::class, 'delete']);
 
-Route::get('/my_page', [Shop_allController::class, 'my_page']);
-Route::post('/my_page', [Shop_allController::class, 'my_page']);
-Route::delete('/my_page', [Shop_allController::class, 'my_page']);
+    Route::get('/my_page', [Shop_allController::class, 'my_page']);
+    Route::post('/my_page', [Shop_allController::class, 'my_page']);
+    Route::delete('/my_page', [Shop_allController::class, 'my_page']);
+});
