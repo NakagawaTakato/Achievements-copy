4d8ab1e2aeb6e8200d4e6a5dc75d7a87ba43cbc6
NakagawaTakato <ntakato6231@gmail.com>
2024-05-14 16:14:40 +0900

    32

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/menu_two';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
